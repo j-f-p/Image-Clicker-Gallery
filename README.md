e38d4a7a1376f00f2f1d6992aaf5fcbbb21c1944
JF Padilla <emailreel@gmail.com>
2018-10-25 11:19:58 -0400

    initialize project from prior version

---
+++ README.md
@@ -0,0 +1,9 @@
+# Cat Clicker
+
+## Abstract
+
+This is a page containing a list of five cats by name. When the user clicks on a name, a profile of that cat is displayed. Each profile contains a cat image and informs the user how many times the user has clicked on the cat image. This page is built with HTML, CSS and JavaScript (ES 6). The script follows the Model-View-Controller software architectural pattern. This page was confirmed with three browsers: Chrome 69, FireFox 62 and Edge 42.
+
+## Project Execution
+
+To run project, open `./index.html` with a recent version of Chrome, FireFox or Edge, such as those listed above.
---
+++ app.js
@@ -0,0 +1,87 @@
+
+const model = {
+  profiles: [
+    {name: "Alpha", image: "alpha.png", numClicks: 0},
+    {name: "Bravo", image: "bravo.jpg", numClicks: 0},
+    {name: "Charlie", image: "charlie.jpg", numClicks: 0},
+    {name: "Delta", image: "delta.jpg", numClicks: 0},
+    {name: "Echo", image: "echo.jpg", numClicks: 0}
+  ]
+};
+
+const view = {
+  selected_i: -1, // a clear declaration with a placeholder initialization
+  navElement: document.getElementsByTagName('nav')[0],
+  mainElement: document.getElementsByTagName('main')[0],
+  imageElement: document.getElementsByClassName('cat-image')[0],
+  nameElement0: document.getElementsByClassName('profile-name')[0],
+  nameElement1: document.getElementsByClassName('profile-name')[1],
+  clickCountElement: document.getElementsByClassName('click-count')[0]
+};
+view.renderProfile = function(index) {
+  const profile = controller.profile(index);
+  view.nameElement0.textContent = profile.name;
+  view.imageElement.style.backgroundImage = `url(${profile.image})`;
+  view.imageElement.className = `cat-image ${index}`;
+  view.nameElement1.textContent = profile.name;
+  view.clickCountElement.textContent = profile.numClicks;
+};
+view.navItemClickResponse = function(event) {
+  const classList = event.target.classList;
+  if (classList.contains('item')===false) {
+    return;
+  }
+  view.navElement.getElementsByClassName('selected')[0]
+      .classList.remove('selected');
+  classList.add('selected');
+  view.selected_i = parseInt( classList.item(1) );
+  view.renderProfile(view.selected_i);
+};
+view.navItemFirstClickResponse = function(event) {
+  const classList = event.target.classList;
+  if (classList.contains('item')===false) {
+    // If target is not a nav item, do nothing.
+    return;
+  }
+  classList.add('selected');
+  view.selected_i = parseInt( classList.item(1) );
+  view.renderProfile(view.selected_i);
+  view.mainElement.style.visibility = 'visible';
+  view.navElement.removeEventListener('click', view.navItemFirstClickResponse);
+  view.navElement.addEventListener('click', view.navItemClickResponse);
+};
+view.renderNavBar = function() {
+  const numProfiles = controller.numProfiles();
+  for(let i=0; i<numProfiles; i++) {
+    const navItem = document.createElement('h2');
+    navItem.textContent = controller.profile(i).name;
+    navItem.className = `item ${i}`;
+    view.navElement.appendChild(navItem);
+  }
+};
+view.initNavBar = function() {
+  view.renderNavBar();
+  view.navElement.addEventListener( 'click', view.navItemFirstClickResponse );
+};
+view.initClickCounter = function() {
+  view.imageElement.addEventListener( 'click', function () {
+    i = parseInt( view.imageElement.classList.item(1) );
+    controller.profile(i).numClicks++;
+    view.clickCountElement.textContent = controller.profile(i).numClicks;
+  });
+};
+
+const controller = {
+  init: function() {
+    view.initNavBar();
+    view.initClickCounter();
+  },
+  numProfiles: function() {
+    return model.profiles.length;
+  },
+  profile: function(index) {
+    return model.profiles[index];
+  }
+};
+
+controller.init();
