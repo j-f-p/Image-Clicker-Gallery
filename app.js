596685e7c0db3512c5b13afc1130d125d3add4ef
JF Padilla <emailreel@gmail.com>
2018-10-30 20:30:03 -0400

    feat, uiux, style: display image source url
    - rename profile key 'name' to 'label' and rename related script and style identifiers accordingly
    - rename profile key 'image' to 'imageFile'
    - add profile key 'imageURL' along with default values
    - display image source url below image frame when image profile is rendered
      - add associated markup, styles and script

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 
 const model = {
   profiles: [
-    {name: "Alpha", image: "alpha.jpg", numClicks: 0},
-    {name: "Bravo", image: "bravo.jpg", numClicks: 0},
-    {name: "Charlie", image: "charlie.jpg", numClicks: 0},
-    {name: "Delta", image: "delta.jpg", numClicks: 0},
-    {name: "Echo", image: "echo.jpg", numClicks: 0}
+    {label: "Alpha", imageFile: "alpha.jpg", numClicks: 0,
+     imageURL: ("https://www.thenational.ae/image/policy:1.776505:1538497810/" +
+                "Na-03-OCT-Hyperloop-NEW.jpg?f=16x9&w=1200&$p$f$w=6c1d176") },
+    {label: "Bravo", imageFile: "bravo.jpg", numClicks: 0,
+     imageURL: ("https://grist.files.wordpress.com/2018/05/gettyimages-" +
+                "176619985-e1535488791582.jpg?w=1024&h=576&crop=1") },
+    {label: "Charlie", imageFile: "charlie.jpg", numClicks: 0,
+     imageURL: "http://i.imgur.com/puDUy9I.jpg" },
+    {label: "Delta", imageFile: "delta.jpg", numClicks: 0,
+     imageURL: ("https://www.aopa.org/-/media/images/aopa-main/" +
+                "news-and-media/2017/november/1120_terrafugia_tfx1/" +
+                "1120_terrafugia_tfx1_16x9.jpg") },
+    {label: "Echo", imageFile: "echo.jpg", numClicks: 0,
+     imageURL: ("https://2gic212zgjlm2bcqpo21qnpk-wpengine.netdna-ssl.com/" +
+                "wp-content/uploads/2018/09/" +
+                "13670806_web1_180928-POI-M-hypersonic.jpg") }
   ]
 };
 
@@ -13,10 +24,11 @@ const view = {
   selected_i: -1, // a clear declaration with a placeholder initialization
   navElement: document.getElementsByTagName('nav')[0],
   mainElement: document.getElementsByTagName('main')[0],
-  nameInTitle: document.getElementsByClassName('profile-name-title')[0],
+  labelInTitle: document.getElementsByClassName('profile-label-title')[0],
   imageElement: document.getElementsByClassName('image')[0],
-  nameInLabel: document.getElementsByClassName('profile-name-text')[0],
+  labelInLabel: document.getElementsByClassName('profile-label-text')[0],
   clickCountElement: document.getElementsByClassName('click-count')[0],
+  imageCreditDiv: document.getElementsByClassName('image-url-div')[0],
   editImageButton: document.getElementsByClassName('edit-image-button')[0],
   alertMessageFrame: null, // placed here for convenience of developer reference
   imageEditFrame: document.getElementsByClassName('image-edit-frame')[0],
@@ -32,20 +44,22 @@ view.closeImageEditFrame = function() {
 };
 view.renderNewImageLabels = function(newImageLabel) {
   view.navElement.children.item(view.selected_i).textContent = newImageLabel;
-  view.nameInTitle.textContent = newImageLabel;
-  view.nameInLabel.textContent = newImageLabel;
+  view.labelInTitle.textContent = newImageLabel;
+  view.labelInLabel.textContent = newImageLabel;
 };
 view.renderNewProfileImage = function(profile) {
-  view.imageElement.style.backgroundImage = `url(${profile.image})`;
+  view.imageElement.style.backgroundImage = `url(${profile.imageFile})`;
   view.clickCountElement.textContent = profile.numClicks;
+  view.imageCreditDiv.firstChild.textContent = 'Source: ' + profile.imageURL;
 };
 view.renderProfile = function() {
   const profile = controller.profile(view.selected_i);
-  view.nameInTitle.textContent = profile.name;
-  view.imageElement.style.backgroundImage = `url(${profile.image})`;
+  view.labelInTitle.textContent = profile.label;
+  view.imageElement.style.backgroundImage = `url(${profile.imageFile})`;
   view.imageElement.className = `image ${view.selected_i}`;
-  view.nameInLabel.textContent = profile.name;
+  view.labelInLabel.textContent = profile.label;
   view.clickCountElement.textContent = profile.numClicks;
+  view.imageCreditDiv.firstChild.textContent = 'Source: ' + profile.imageURL;
   if( view.imageEditFrame.style.visibility==='visible' ) {
     view.closeImageEditFrame();
   }
@@ -78,7 +92,7 @@ view.initRenderNavBar = function() {
   const numProfiles = controller.numProfiles();
   for(let i=0; i<numProfiles; i++) {
     const navItem = document.createElement('h2');
-    navItem.textContent = controller.profile(i).name;
+    navItem.textContent = controller.profile(i).label;
     navItem.className = `item ${i}`;
     view.navElement.appendChild(navItem);
   }
@@ -118,17 +132,18 @@ view.initSaveEditButton = function() {
       event.preventDefault();
       const profile = controller.profile(view.selected_i);
 
-      if(view.newImageLabelElement.value!=profile.name &&
+      if(view.newImageLabelElement.value!=profile.label &&
          view.newImageLabelElement.value!='') {
-        profile.name = view.newImageLabelElement.value;
-        view.renderNewImageLabels(profile.name);
-      } // Otherwise, keep name.
+        profile.label = view.newImageLabelElement.value;
+        view.renderNewImageLabels(profile.label);
+      } // Otherwise, keep label.
 
-      if(view.newImageURLelement.value!=profile.image) {
+      if(view.newImageURLelement.value!=profile.imageFile) {
         // Proceed only if image url is unique.
-        profile.image = view.newImageURLelement.value;
+        profile.imageFile = view.newImageURLelement.value;
         profile.numClicks = 0;
         view.closeImageEditFrame();
+        profile.imageURL = profile.imageFile;
         view.renderNewProfileImage(profile);
       } else {
         // Close form.
